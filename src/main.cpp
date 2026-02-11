86fcb145923c135c7494b4230380d85628b507df
willmo3 <thelordofbouge@gmail.com>
2026-02-11 14:24:52 -0500

    Restore to no serialization + intersection

--- src/main.cpp
+++ src/main.cpp
@@ -4,11 +4,5 @@
 #include "MixedForm/MixedForm.hpp"
 
 int main() {
-    // Quick I/O sanity test.
-    std::ofstream file("out.json");
-    cereal::JSONOutputArchive archive(file);
-
-    MixedForm w = MixedForm(AffineForm(Winterval(-2, 3)), Winterval(-1, 2));
-    archive(w);
     return 0;
 }
